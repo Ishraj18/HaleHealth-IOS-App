0bf508e2910c5ce89ce0609bfb4495aab073d2b6
Ishraj18 <65031670+Ishraj18@users.noreply.github.com>
2026-06-09 17:50:59 +0530

    Initial Commit

---
+++ halehealth/halehealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  halehealthApp.swift
+//  halehealth
+//
+//  Created by Ishraj Singh on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct halehealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
